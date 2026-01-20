c9de647b2babbb5c615974f0b7780d66d2949488
theholybull <theholybull@gmail.com>
2026-01-20 09:34:00 -0500

    CT-2026-01-20-RT-003: stop tracking ROS build/install/log artifacts; ignore runtime caches

--- robot/ros_ws/build/kilo_core/share/kilo_core/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/opt/kilo7/robot/ros_ws/build/kilo_core"
